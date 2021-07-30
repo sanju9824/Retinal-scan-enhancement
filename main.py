39025b0356fe79a4f2078ede22cb6ca49efe0012
Sanjana <88078551+sanju9824@users.noreply.github.com>
2021-07-30 13:11:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# Import
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# Parameters
+IMG_SIZE=512
+img_name='test_img1'
+img_path='Inputs/'+img_name+'.jpeg'
+def enhance(path, sigmaX=10):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = crop_image_from_gray(image)
+    image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
+    image = cv2.addWeighted(image, 4, cv2.GaussianBlur(image, (0, 0), sigmaX), -4, 128)
+
+    return image
+
+def crop_image_from_gray(img, tol=7):
+    if img.ndim == 2:
+        mask = img > tol
+        return img[np.ix_(mask.any(1), mask.any(0))]
+    elif img.ndim == 3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        mask = gray_img > tol
+
+        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
+        if (check_shape == 0):  # image is too dark so that we crop out everything,
+            return img  # return original image
+        else:
+            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+            img = np.stack([img1, img2, img3], axis=-1)
+        return img
+
+if __name__=='__main__':
+    fig = plt.figure(figsize=(25, 16))
+    path = img_path
+    image = enhance(path, sigmaX=30)
+    try:
+        os.mkdir('Results')
+    except FileExistsError:
+        pass
+    cv2.imwrite('Results/'+img_name+'.jpeg',image)
+
+
